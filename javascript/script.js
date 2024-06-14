3e1d79a94531e933a910c84f98582c21733d4aff
Thomas Lousada <78939209+Thomasdotcom@users.noreply.github.com>
2024-06-14 13:40:59 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,2 @@
+alert('Você acaba de cair em um phishing !');
+alert('Mesmo sendo um colega de trabalho, desconfie de links enviados, alguém pode ter hackeado sua conta e estar se passando por essa pessoa')
